02713b168dc17e32a5785d71cc4e5452586c8f6f
MeansKane <phamminhkhangpy2003@gmail.com>
2024-07-22 15:12:40 +0700

    declare and initialize widgets in second activity

--- app/src/main/java/com/example/thelotteryapp/SecondActivity.kt
+++ app/src/main/java/com/example/thelotteryapp/SecondActivity.kt
@@ -1,12 +1,17 @@
 package com.example.thelotteryapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SecondActivity : AppCompatActivity() {
+    private lateinit var luckyNumbers: TextView
+    private lateinit var shareBtn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,8 @@ class SecondActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        luckyNumbers = findViewById(R.id.luckyNumbers)
+        shareBtn = findViewById(R.id.shareBtn)
     }
 }
